453a43593fb47ca378de17534ea24582f6c7dfe2
karthikeyan <karthirajakarthik.k@gmail.com>
2019-08-11 06:59:31 +0530

    this is my first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const disttrixs=require('@karthikeyan_mean/disttrixs100');
+  disttrixs.inviteUsersWithin100kms('Customers _Assignment_Coding Challenge (Upto 6 Years) (2).txt',(err,result)=>
+  {
+    console.log(result);
+  });
